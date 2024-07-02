38c9e4a2faeb55a27a308bcabd990eac608bd82c
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-07-01 22:59:12 -0700

    Create deletedb.sql

---
+++ config/deletedb.sql
@@ -0,0 +1,14 @@
+-- Simply delete everything from your DB :)
+
+-- Delete `users` table if exists
+DROP TABLE IF EXISTS `users`;
+-- Delete `civilians` table if exists
+DROP TABLE IF EXISTS `civilians`;
+-- Delete `arrests` table if exists
+DROP TABLE IF EXISTS `arrests`;
+-- Delete `incidents` table if exists
+DROP TABLE IF EXISTS `incidents`;
+-- Delete `reports` table if exists
+DROP TABLE IF EXISTS `reports`;
+-- Delete `tickets` table if exists
+DROP TABLE IF EXISTS `tickets`;
